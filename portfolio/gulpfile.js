756742264674cbe815e43598919986d193d81088
Erik Erikson <erik.erikson@analogfolk.com>
2015-05-03 23:16:25 -0400

    Performance stuff

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jshint = require('gulp-jshint');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 var CONFIG = {
 
@@ -25,14 +27,27 @@ gulp.task('watch', function() {
     gulp.watch('./src/js/**/*.*', ['js-hint']);
 });
 
+gulp.task( 'css-minify', function () {
+	gulp.src( CONFIG.styleDestination + "application.css" )
+        .pipe(minifyCss())
+        .pipe(gulp.dest( CONFIG.styleDestination ));
+});
+
 gulp.task( 'sass', function () {
 	gulp.src( CONFIG.styleSrc )
         .pipe(sass())
         .pipe(gulp.dest( CONFIG.styleDestination ));
 });
 
+
 gulp.task( 'js-hint', function () {
 	return gulp.src(CONFIG.jsSrc)
 	    .pipe(jshint())
+    	.pipe(gulp.dest( CONFIG.jsDestination ));
+});
+
+gulp.task( 'js-uglify', function () {
+	return gulp.src(CONFIG.jsSrc)
+	    .pipe(uglify())
     	.pipe(gulp.dest( CONFIG.jsDestination ));
 });
